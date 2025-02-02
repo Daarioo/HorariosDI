ffc9aae4c5b65a722f0cb219f4a132d77474b209
Marcoscv <marcoscvwork@gmail.com>
2025-02-02 21:11:36 +0100

    02/02/2025
    casa

--- Interfaz/movil/js/nav.js
+++ Interfaz/movil/js/nav.js
@@ -8,6 +8,23 @@ const profesoresFrame = document.getElementById("profesoresFrame");
 
 //Guardar los botones en variables
 const menuToggle = document.getElementById("menuToggle")
+
+menuToggle.addEventListener('click', function() {
+    const navPanel = document.getElementById('navPanel');
+    navPanel.classList.toggle('active');
+});
+
+// Establecer emptyFrame como visible por defecto
+emptyFrame.style.display = "block";
+ciclosFrame.style.display = "none";
+modulosFrame.style.display = "none";
+horariosFrame.style.display = "none";
+alumnosFrame.style.display = "none";
+profesoresFrame.style.display = "none";
+
+
+//Guardar los botones en variables
+
 const ciclos= document.getElementById("ciclosBtn");
 const modulos = document.getElementById("modulosBtn");
 const horarios = document.getElementById("horariosBtn");
@@ -16,12 +33,53 @@ const profesores = document.getElementById("profesoresBtn");
 const logoutBtn = document.getElementById("logoutBtn");
 
 
-menuToggle.addEventListener('click', function() {
-    const navPanel = document.getElementById('navPanel');
-    navPanel.classList.toggle('active');
-});
+
 
 logoutBtn.addEventListener('click', function(event) {
-    event.preventDefault(); // Evita el comportamiento predeterminado del formulario
     window.location.href = 'index.html'; // Redirige al usuario
+});
+
+ciclos.addEventListener('click', function(event) {
+    emptyFrame.style.display = "none";
+    ciclosFrame.style.display = "block";
+    modulosFrame.style.display = "none";
+    horariosFrame.style.display = "none";
+    alumnosFrame.style.display = "none";
+    profesoresFrame.style.display = "none";
+});
+
+modulos.addEventListener('click', function(event) {
+    emptyFrame.style.display = "none";
+    ciclosFrame.style.display = "none";
+    modulosFrame.style.display = "block";
+    horariosFrame.style.display = "none";
+    alumnosFrame.style.display = "none";
+    profesoresFrame.style.display = "none";
+});
+
+horarios.addEventListener('click', function(event) {
+    emptyFrame.style.display = "none";
+    ciclosFrame.style.display = "none";
+    modulosFrame.style.display = "none";
+    horariosFrame.style.display = "block";
+    alumnosFrame.style.display = "none";
+    profesoresFrame.style.display = "none";
+});
+
+alumnos.addEventListener('click', function(event) {   
+    emptyFrame.style.display = "none";
+    ciclosFrame.style.display = "none";
+    modulosFrame.style.display = "none";
+    horariosFrame.style.display = "none";
+    alumnosFrame.style.display = "block";
+    profesoresFrame.style.display = "none";
+});
+
+profesores.addEventListener('click', function(event) {
+    emptyFrame.style.display = "none";
+    ciclosFrame.style.display = "none";
+    modulosFrame.style.display = "none";
+    horariosFrame.style.display = "none";
+    alumnosFrame.style.display = "none";
+    profesoresFrame.style.display = "block";
 });
